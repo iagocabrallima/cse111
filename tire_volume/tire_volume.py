d4dfe3ff86aeb4e793b78c8266c7ee9cca7cbba2
iagol lima <iagol360@gmail.com>
2022-09-25 02:23:08 -0300

    Requirements

--- tire_volume/tire_volume.py
+++ tire_volume/tire_volume.py
@@ -1,8 +1,20 @@
-print('Lets see your tire volume?')
+#Imports
+from datetime import datetime
+from turtle import rt
 
+#Variables
 width=int(input('Enter the width of the tire in mm (ex 205):'))
 ratio=int(input('Enter the aspect ratio of the tire (ex 60):'))
 diameter=int(input('Enter the diameter of the wheel in inches (ex 15):'))
 
+#Calculate
 v=round((3.14*(width**2)*ratio*((width*ratio)+(2540*diameter)))/10000000000,2)
-print(f'The approximate volume is {v} liters')
+print(f"The approximate volume is {v} liters")
+#Time
+current_date_and_time = datetime.now()
+
+
+with open('volume.txt',mode='at') as volumes_file:
+    print(f"{current_date_and_time:%Y-%m-%d}{width},{ratio},{diameter},{v}", file=volumes_file)
+with open('volume.txt',mode='rt') as volumes_file:
+    print(volumes_file.read())
